2b3506114b2f878a8ae61d5435e907a015e65470
Josiah-Davis <davisj6566@student.faytechcc.edu>
2026-02-05 15:58:31 +0000

    Edited

--- p1lab1_Davis.py
+++ p1lab1_Davis.py
@@ -1,9 +1,16 @@
-# CtI 110
+# CTI 110
 # P1LAB1
 # Your Name
 # 2/5/26
 # Practice Python Program
 
+# name = "bob"
+# greet the user
+print("Hi, what's your first name ")
+first_name = input()
+last_name = input("What's Last your name? ")
 name = "bob"
-print ("Nice to meet you", name)
-print() # extra blank line
+print ("Nice to meet you", first_name, last_name)
+print()
+hobby = input("What type of 'stuff' do you like? ")
+print("I'm a computer but I would probably like", hobby, "too.")
